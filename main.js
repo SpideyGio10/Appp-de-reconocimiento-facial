75324505190610adc1353557bf64fdc639b97eee
SpideyGio10 <104469485+SpideyGio10@users.noreply.github.com>
2023-04-11 15:16:42 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -9,5 +9,15 @@ camera=document.getElementById("camera");
 Webcam.attach('#camera');
 
 function take_snapshot(){
-    
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image" src="'+data_uri+'"/>';
+    })
+}
+
+console.log('ml5 version:',ml5.version);
+
+classifier=ml5.imageClassifier('',modelLoaded)
+
+function modelLoaded(){
+    console.log('¡Modelo cargado!');
 }
