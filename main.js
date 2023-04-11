06c2a028687879ac66b0854251601eceb522c8da
SpideyGio10 <104469485+SpideyGio10@users.noreply.github.com>
2023-04-11 14:20:53 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,13 @@
-camera=document.getElementById("camera");
-Webcam.attach('#camera');
 Webcam.set({
     width:350,
     height:300,
     image_format:'png',
     png_quality:90
 });
+
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
 function take_snapshot(){
-    Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'"/>';
-    })
-}
-console.log('ml5 versión:',ml5.version);
-classifier = ml5.imageClassifier('',modelLoaded);
-function modelLoaded(){
-    console.log('modelo cargado');
+    
 }
